44976e71d66750350305b4b57a18ac28fd0dbe24
Kevin J Mosley <Kevinjmosley93@gmail.com>
2023-10-10 00:18:43 -0500

    remove extra files

--- frontend-dev/components/utils/index.js
+++
@@ -1,9 +0,0 @@
-import { Liquid } from "liquidjs";
-
-export default engine = new Liquid({
-  root: path.resolve(__dirname, "components"), // root for layouts/includes lookup
-  extname: ".liquid", // used for layouts/includes, defaults ""
-});
-engine
-  .renderFile("hello", { name: "alice" }) // will read and render `views/hello.liquid`
-  .then(console.log);
